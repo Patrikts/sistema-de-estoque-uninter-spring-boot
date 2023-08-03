8d1a9573d1d7f0801bef56a49323a1db56970f40
Patrik <trikteixeira@gmail.com>
2023-08-03 16:42:10 -0300

    Criando a classe de acesso a dados

---
+++ src/main/java/br/com/springboot/dao/CRUD.java
@@ -0,0 +1,13 @@
+package br.com.springboot.dao;
+
+import java.util.List;
+
+public interface CRUD<T, ID> {
+	
+	T pesquisaPeloId(ID id);
+	List<T> lista();
+	void insere(T t);
+	void atualiza(T t);
+	void remove(T t);
+
+}
--- src/main/java/br/com/springboot/model/Cliente.java
+++ src/main/java/br/com/springboot/model/Cliente.java
@@ -2,18 +2,40 @@ package br.com.springboot.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="clientes")
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false, length = 50)
 	private String nome;
+	@Column(length = 11)
 	private String cpf;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+	@Column(name = "data_nascimento", columnDefinition = "DATE")
 	private LocalDate dataDeNascimento;
+	@Enumerated(EnumType.STRING)
 	private Sexo sexo;
+	@Column(length = 10)
+	private String telefone;
+	@Column(length = 11)
 	private String celular;
+	@Column(length = 50)
 	private String email;
 	private boolean ativo;
-	
-	
 	
 	public Long getId() {
 		return id;
@@ -53,6 +75,14 @@ public class Cliente {
 
 	public void setSexo(Sexo sexo) {
 		this.sexo = sexo;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
 	}
 
 	public String getCelular() {
