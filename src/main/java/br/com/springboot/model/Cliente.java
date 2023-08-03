bc431eff4e3ccfa29c3052b2f0157df87af5bab5
Patrik <trikteixeira@gmail.com>
2023-08-03 14:11:21 -0300

    Criando a model

---
+++ src/main/java/br/com/springboot/model/Cliente.java
@@ -0,0 +1,82 @@
+package br.com.springboot.model;
+
+import java.time.LocalDate;
+
+public class Cliente {
+
+	private Long id;
+	private String nome;
+	private String cpf;
+	private LocalDate dataDeNascimento;
+	private Sexo sexo;
+	private String celular;
+	private String email;
+	private boolean ativo;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public LocalDate getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(LocalDate dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+	public Sexo getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+}
---
+++ src/main/java/br/com/springboot/model/Sexo.java
@@ -0,0 +1,19 @@
+package br.com.springboot.model;
+
+public enum Sexo {
+
+	MASCULINO("Masculino"),
+	FEMININO("Feminino");
+	
+	private String descricao;
+	
+	Sexo(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	
+}
