af68989f3bd6702fefe28c03f3efb588296c64b5
Patrik <trikteixeira@gmail.com>
2023-08-04 11:17:19 -0300

    Implementando as funcionalidades da tela de gerenciamento

--- src/main/java/br/com/springboot/controller/ClienteController.java
+++ src/main/java/br/com/springboot/controller/ClienteController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,6 +29,32 @@ public class ClienteController {
 	public String salva(@ModelAttribute Cliente cliente) {
 		bo.insere(cliente);
 		return "/cliente/formulario";
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public ModelAndView lista(ModelMap model) {
+		model.addAttribute("cliente", bo.listaTodos());
+		return new ModelAndView("/cliente/lista", model);
+	}
+	
+	@RequestMapping(value = "/edita/{id}", method = RequestMethod.GET)
+	public ModelAndView edita(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cliente", bo.pesquisaPeloId(id));
+		return new ModelAndView("/cliente/formulario", model);
+	}
+	
+	@RequestMapping(value = "/inativa/{id}", method = RequestMethod.GET)
+	public String inativa(@PathVariable("id") Long id) {
+		Cliente cliente = bo.pesquisaPeloId(id);
+		bo.inativa(cliente);
+		return "redirect:/clientes";
+	}
+	
+	@RequestMapping(value = "/ativa/{id}", method = RequestMethod.GET)
+	public String ativa(@PathVariable("id") Long id) {
+		Cliente cliente = bo.pesquisaPeloId(id);
+		bo.ativa(cliente);
+		return "redirect:/clientes";
 	}
 
 }
